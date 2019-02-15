3448f93473f33cbded29e26337bb159483cd9c0f
mr-rampage <readonly@che.wbac.ca>
2019-02-14 21:58:40 -0500

    feat: adding return tests

--- spec/es6-generators.js
+++ spec/es6-generators.js
@@ -1,19 +1,50 @@
 describe('ES6 generators', () => {
-  const FILL_IN_THE_BLANK = '🤔';
+  const FILL_IN_THE_BLANK = null
 
   describe('introduction', () => {
+    const fixture = function* () {
+      yield 'Hello'
+      yield 'World'
+    }
 
     it('should return a result when called', () => {
-      const fixture = function* () {
-        yield 'Hello';
-        yield 'World';
-      };
+      const actual = fixture()
+      expect(actual.next()).toEqual({value: 'Hello', done: false})
+    })
 
-      const actual = fixture();
+    it('should return ??? when called twice', () => {
+      const actual = fixture()
+      actual.next()
+      expect(actual.next()).toEqual(FILL_IN_THE_BLANK)
+    })
 
-      expect(actual.next()).toBe(FILL_IN_THE_BLANK);
-    });
+    it('should return ??? when called thrice', () => {
+      const actual = fixture()
+      actual.next()
+      actual.next()
+      expect(actual.next()).toEqual(FILL_IN_THE_BLANK)
+    })
+  })
 
-  });
+  describe('Generator', () => {
+    const generator = function* () {
+      yield 1
+      yield 2
+      yield 3
+    }
 
-});
+    it('should return ??? when return is called', () => {
+      const actual = generator()
+      actual.next()
+      expect(actual.return('foo')).toEqual(FILL_IN_THE_BLANK)
+    })
+
+    it('should return ??? when return is called', () => {
+      const actual = generator()
+      actual.next()
+      actual.return('foo')
+      expect(actual.next()).toEqual(FILL_IN_THE_BLANK)
+    })
+  })
+
+})
